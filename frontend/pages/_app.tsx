e4b4f2c5f4c925fd3a841db75646785de12eb1fd
heyitskoi <112921161+heyitskoi@users.noreply.github.com>
2025-06-06 11:35:48 +0200

    setup tailwind

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app';
+import '../styles/globals.css';
 import { AuthProvider } from '../lib/AuthContext';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
