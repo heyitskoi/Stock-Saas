c6612a922be316220debc6c38437d429c359555b
heyitskoi <112921161+heyitskoi@users.noreply.github.com>
2025-06-06 17:40:41 +0200

    fix: enable dark mode toggle

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,11 +1,14 @@
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 import { AuthProvider } from '@/lib/auth-context';
+import { ThemeProvider } from '@/components/theme-provider';
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
-      <Component {...pageProps} />
+      <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <Component {...pageProps} />
+      </ThemeProvider>
     </AuthProvider>
   );
 }
